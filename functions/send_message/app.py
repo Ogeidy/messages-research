09d79a8333553e6e2d2e687ba60eea14e0da8b6a
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-09-13 22:27:12 +0300

    functions: send_message: add table_name

--- functions/send_message/app.py
+++ functions/send_message/app.py
@@ -1,20 +1,25 @@
+import os
 import json
 import uuid
 
 import boto3
 
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource("dynamodb")
-    messages = dynamodb.Table("messages")
+db = boto3.resource("dynamodb")
+table_name = os.environ["MESSAGES_TABLE_NAME"]
+messages_table = db.Table(table_name)
 
+
+def lambda_handler(event, context):
     if event.get("body", False):
         message_id = str(uuid.uuid4())
+
         if isinstance(event["body"], dict):
             body = event["body"]
         else:
             body = json.loads(event["body"].replace("'", '"'))
-        print(f"{event['body']=}, {message_id=}, {body=} *")
-        messages.put_item(Item={"message_id": message_id, "message": body["message"]})
 
-    return {"statusCode": 200, "body": json.dumps("OK")}
+        print(f"{event['body']=}, {message_id=}, {body=} *")
+        messages_table.put_item(Item={"id": message_id, "message": body["message"]})
+
+    return {"statusCode": 200, "body": json.dumps({"id": message_id})}
