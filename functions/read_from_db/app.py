7d1b25cdc1766d7df002afc6a654d8f1f432e46f
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-09-13 22:26:18 +0300

    functions: add 'read_from_db' lambda function

---
+++ functions/read_from_db/app.py
@@ -0,0 +1,29 @@
+import os
+import json
+import uuid
+
+import boto3
+
+
+db = boto3.resource("dynamodb")
+table_name = os.environ["MESSAGES_TABLE_NAME"]
+messages_table = db.Table(table_name)
+
+sqs = boto3.resource("sqs")
+queue_name = os.environ["MESSAGES_QUEUE_NAME"]
+messages_queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+
+def lambda_handler(event, context):
+    resp = messages_table.scan()
+
+    for msg in resp["Items"]:
+        print(f"{msg['message_id']=}, {msg['message']=}")
+        sqs_resp = messages_queue.send_message(
+            MessageGroupId="1",
+            MessageDeduplicationId=str(uuid.uuid4()),
+            MessageBody=(msg["message"]),
+        )
+        print(f"{sqs_resp=}")
+
+    return {"statusCode": 200, "body": json.dumps({"status": "OK", "n_items": len(resp["Items"])})}
