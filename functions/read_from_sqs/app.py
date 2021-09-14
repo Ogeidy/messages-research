90258c3e9221d4d414acac52fd2aaf1103e49c7c
Yakov Mamontov <yakov-mamontov@yandex.ru>
2021-09-14 23:49:29 +0300

    functions: read_from_sqs: fix receive_messages

--- functions/read_from_sqs/app.py
+++ functions/read_from_sqs/app.py
@@ -9,23 +9,23 @@ queue_name = os.environ["MESSAGES_QUEUE_NAME"]
 messages_queue = sqs.get_queue_by_name(QueueName=queue_name)
 
 def lambda_handler(event, context):
-    response = messages_queue.receive_message(
+    response = messages_queue.receive_messages(
         MaxNumberOfMessages=5, MessageAttributeNames=["All"], VisibilityTimeout=1, WaitTimeSeconds=0
     )
 
     print(f"{response=}")
 
-    if not response.get("Messages", False):
+    if not response:
         return {"statusCode": 200, "body": json.dumps({"messages": []})}
 
     messages = []
-    for msg in response["Messages"]:
-        receipt_handle = msg["ReceiptHandle"]
-        msg_body = msg["Body"]
+    for msg in response:
+        msg_body = msg.body
+        receipt_handle = msg.receipt_handle
 
         print(f"{msg_body=}")
 
-        messages_queue.delete_message(ReceiptHandle=receipt_handle)
+        messages_queue.delete_messages(Entries=[{"Id": "no", "ReceiptHandle": receipt_handle}])
 
         messages.append(msg_body)
 
